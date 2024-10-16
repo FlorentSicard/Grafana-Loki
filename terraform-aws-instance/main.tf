893b3e0327f39560c0dc9c5fe81dcdce28b2d5d2
Florent Sicard <sicardflorent@hotmail.fr>
2024-10-16 11:55:06 +0200

    config main.tf load blancer, S3 target groupe & listener

---
+++ terraform-aws-instance/main.tf
@@ -0,0 +1,79 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-west-3"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-045a8ab02aadf4f88"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "loki"
+  }
+}
+
+resource "random_pet" "bucket_name" {
+  length    = 5
+  separator = "-"
+  prefix    = "learning"
+}
+
+module "s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.1"
+
+  bucket = random_pet.bucket_name.id
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerPreferred"
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_lb_target_group" "loki_tg" { // Target Group A
+ name     = "target-group_loki"
+ port     = 80
+ protocol = "HTTP"
+}
+
+resource "aws_lb_target_group_attachment" "tg_attachment" {
+ target_group_arn = aws_lb_target_group.my_tg.arn
+ target_id        = aws_instance.instance.id
+ port             = 80
+}
+
+resource "aws_lb" "loki_lb" {
+ name               = "loki_lb"
+ internal           = false
+ load_balancer_type = "application"
+ 
+ tags = {
+   Environment = "dev"
+ }
+}
+
+resource "aws_lb_listener" "loki_listener" {
+ load_balancer_arn = aws_lb.loki_lb
+ port              = "80"
+ protocol          = "HTTP"
+
+ default_action {
+   type             = "forward"
+   target_group_arn = aws_lb_target_group.loki_tg_a.arn
+ }
+}
+
